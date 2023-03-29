9cdfe940df6045a7e8932a0f3e9ef952b43ff426
TommyArias <arias.tommy1009@gmail.com>
2023-03-29 09:37:00 +0200

    all

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-body=document.querySelector('body')
-background=document.querySelector('background')
-logoandline=document.querySelector('logoandline')
-video=document.querySelector('video')
-triangle1=document.querySelector('triangle1')
-triangle2=document.querySelector('triangle2')
-abc=document.querySelector('abc')
-xyz=document.querySelector('xyz')
-acceuil=document.querySelector('acceuil')
-
-
-
 
 button.onclick = function nique() {
-    setTimeout(b, 100)
-    setTimeout(c, 600)
-    setTimeout(b, 1100)
-    setTimeout(c, 1600)
-    setTimeout(b, 2100)
-    setTimeout(c, 2600)
-    setTimeout(a, 3100)
+    setTimeout(a, 500)
   };
 
   function a() {
-    body.style.display = 'none';
     window.location.replace("./darkPage.html")
-  }
-
-  function b() {
-    body.style.backgroundColor = 'red';
-  }
-
-  function c() {
-    body.style.backgroundColor = 'white';
   }
 
 
