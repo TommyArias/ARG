579f7c09487b670b5d04dba865bba709eb87a725
GET MOUSSED <mofanafoussa@gmail.com>
2023-04-20 12:52:00 +0200

    i

---
+++ old/README.md
@@ -0,0 +1,4 @@
+Installer les dependances
+`pnpm i` ou `npm i` si ta pas pnpm
+
+`pnpm run dev` ou `npm run dev`
---
+++ old/Script/script.js
@@ -0,0 +1,11 @@
+
+button.onclick = function nique() {
+    setTimeout(a, 500)
+  };
+
+  function a() {
+    window.location.replace("./darkPage.html")
+  }
+
+
+  console.log("OK")
