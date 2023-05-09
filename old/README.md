ccf1e7de5958d95e283f01fc0ba62d675041ec36
GET MOUSSED <mofanafoussa@gmail.com>
2023-05-09 10:52:19 +0200

    added documents page

--- old/README.md
+++
@@ -1,4 +0,0 @@
-Installer les dependances
-`pnpm i` ou `npm i` si ta pas pnpm
-
-`pnpm run dev` ou `npm run dev`
--- old/Script/main.js
+++
@@ -1,5 +0,0 @@
-function redirectToDark() {
-  window.location.href = "./darkPage.html";
-}
-
-document.querySelector("#button").onclick = redirectToDark;
